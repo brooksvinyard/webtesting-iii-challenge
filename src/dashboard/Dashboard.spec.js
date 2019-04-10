a081b06d4538550eb096980245295cffaaaf7ee3
brooksvinyard <8202653+brooksvinyard@users.noreply.github.com>
2019-04-10 14:45:14 -0700

    Add tests to controls

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -11,6 +11,7 @@ import renderer from 'react-test-renderer';
 import 'react-testing-library/cleanup-after-each'; 
 
 import Dashboard from './Dashboard';
+import Controls from '../controls/Controls';
 
 
 describe('<Dashboard />', () => {
@@ -21,3 +22,49 @@ describe('<Dashboard />', () => {
     expect(tree).toMatchSnapshot();
   });
 });
+
+describe('<Controls />', () => {
+
+    it('should render default buttons', () => {
+        const { getByText } = render(<Dashboard />);
+    
+        getByText(/lock gate/i);
+        getByText(/close gate/i);
+    });
+
+    it('should change state on button clicks', () => {
+        const { getByText } = render(<Dashboard />);
+    
+        const lockButton = getByText(/lock gate/i);
+        const closeButton = getByText(/close gate/i);
+
+        fireEvent.click(lockButton);
+        fireEvent.click(closeButton);
+
+        getByText(/unlocked/i);
+        getByText(/open/i);
+    });
+
+    it('should disable the closed button if the gate is locked and closed', () => {
+        const { getByText } = render(<Dashboard />);
+    
+        const closeButton = getByText(/close gate/i);
+        const lockButton = getByText(/lock gate/i);
+
+        // Close and lock the gate
+        fireEvent.click(closeButton);
+        fireEvent.click(lockButton);
+
+        // Cannot open the gate
+        const openButton = getByText(/open gate/i);
+        expect( openButton.disabled ).toEqual(true);
+    });
+
+    it('should disable the lock button if the gate is open', () => {
+        const { getByText } = render(<Dashboard />);
+
+        // The gate starts open so lick should be disabled
+        const lockButton = getByText(/lock gate/i);
+        expect( lockButton.disabled ).toEqual(true);
+    });
+  });
