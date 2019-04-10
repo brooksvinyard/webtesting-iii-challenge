09d1a568fcd61859d820edbb4def8f34ff313fa9
brooksvinyard <8202653+brooksvinyard@users.noreply.github.com>
2019-04-10 13:34:02 -0700

    Initial commit

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,23 @@
 // Test away
+
+// Gate
+// defaults to unlocked and open
+// cannot be closed or opened if it is locked
+
+
+import React from 'react';
+import { render, fireEvent, cleanup } from 'react-testing-library';
+import renderer from 'react-test-renderer'; 
+import 'react-testing-library/cleanup-after-each'; 
+
+import Dashboard from './Dashboard';
+
+
+describe('<Dashboard />', () => {
+
+  it('matches snapshot', () => {
+    const tree = renderer.create(<Dashboard></Dashboard>).toJSON();
+
+    expect(tree).toMatchSnapshot();
+  });
+});
